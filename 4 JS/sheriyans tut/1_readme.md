560a3dfbdc0cddfc59e4f055848d029daaa5ac5d
Niraj Kumar <nirajs9kr@gmail.com>
2023-09-17 18:58:24 +0530

    .

---
+++ 4 JS/sheriyans tut/1_readme.md
@@ -0,0 +1,17 @@
+1 js 
+2 -word vs keyword
+3 -var const let 
+4 --the difference
+5 --browser context api
+6 --window object 
+7 --stack
+8 --heap memory
+9 --execution context 
+10 --hosting
+11 --lexical environment
+12 --types in js 
+13 --how to copy reference values
+14 --conditionals 
+15 --if else else-if
+16 --truthy and falsy 
+17 --switch
---
+++ 4 JS/sheriyans tut/1script.js
@@ -0,0 +1,31 @@
+// word vs keyword
+// chacha=word
+// for=keyword
+
+// var const and let
+// variables and const
+
+//hoisting- variable and functions are hoisted which means there declation is moved on the top of code 
+
+var a;
+console.log(a); //we will get undifned but not error
+a=21;
+
+// types in js
+// 1.primittive =number, string , bool , undefined
+// 2. reference [] () {} aisi kooi bhi value jisko copy karne par real copy nhi hota, balki us main value ka reference pass ho jaat hai, use hum referemce value kahte hai, aur jisko copy karne par real copy ho jaata hia wo premittive type value hoti hai
+
+var a=12;
+var b=a;
+b= b+2;
+var a=[1,23,4,5];
+var b=a;
+b.pop(); 
+
+//conditional - if else if-else
+// loop
+
+ 
+
+
+
