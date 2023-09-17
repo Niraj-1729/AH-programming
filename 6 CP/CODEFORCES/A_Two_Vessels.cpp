8225d822a9614d99016c1320211e0d73488f2f1b
Niraj Kumar <nirajs9kr@gmail.com>
2023-09-17 09:29:58 +0530

    .

---
+++ 6 CP/CODEFORCES/A_Two_Vessels.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c;
+        if (a>b)
+        {
+            d=a-b;
+            if (d>c)
+            {
+              if (c%d==0)
+              {
+                cout<<c/2)/;
+              }
+                
+            }
+            else if (c%d!=0)
+            {
+                 
+            }
+            
+            else{
+                cout<<1;
+            }
+            
+        }
+        else if (b>a)
+        {
+            d=a-b;
+        }
+    }
+
+    return 0;
+}
