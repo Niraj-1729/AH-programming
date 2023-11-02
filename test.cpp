3651dfc40a88e5bf56e56d59d94a80de101fdd54
Niraj Kumar <nirajs9kr@gmail.com>
2023-11-02 11:53:19 +0530

    .

--- test.cpp
+++ test.cpp
@@ -1,11 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n=5;
-    int *ptr = &n;
-    cout<<n<<" "<<&n<<" "<<ptr<<" "<<*ptr;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        if (sum % n != 0)
+        {
+            cout << "NO" << endl;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            /* code */
+            if (sum % 2 != arr[i] % 2)
+            {
+                cout << "NO" << endl;
+            }
+        }
+        cout<<"YES"<<endl;
+    }
 
     return 0;
 }
